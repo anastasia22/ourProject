eaab5dfa60ed7a627ee91f4e580f866b951fd1c5
unknown <abuniak22@gmail.com>
2014-12-01 22:07:03 +0200

    added template skeleton

--- js/contentTemplates.js
+++ js/contentTemplates.js
@@ -4,7 +4,9 @@
 /*templates for all sections HOME HELP MOVIES . every menu button has its own template*/
 var apiKey = '7a135ff2c408f8e138e4645f83b30222';
 var baseUrl = 'http://api.themoviedb.org/3';
-var posterUrl = 'http://image.tmdb.org/t/p/w300/'
+var bigImageUrl = 'http://image.tmdb.org/t/p/w300/';
+var mediumImageUrl = 'http://image.tmdb.org/t/p/w185/';
+var smallImageUrl = 'http://image.tmdb.org/t/p/w185/';
 // HOME BLOCK
 function homeTemplate() {
     var newsPictures = {
@@ -73,31 +75,90 @@ function moviesTemplate() {
     $('#mainContent').append(movieBlocks);
 }
 
-function singleMoviePage(){
+function singleMoviePage(id){
+    var singleMovieTemplate = 
+    '<section id = "singleMovie">' + 
+    '<h1> <%= singleMovie.title %> </h1>' +
+    // '<img src="<%= mediumImageUrl %>' + '<%= singleMovie.poster %>" >' +
+    '<q> <%= singleMovie.tagline %> </q>' +
+    '<ul> <li>Rates: <%= singleMovie.rating %> </li>' +
+    '<li>Year: <%= singleMovie.year %> </li>' +
+    '<li>Runtime:<%= singleMovie.runtime %> </li>' +
+    '<li>Country:' +  
+        '<% _.each(singleMovie.countries, function(el, i){ %>' + 
+        '<%= singleMovie.countries[i] %>' +
+        '<% }) %> </li></ul>' +
+    '<p> <%= singleMovie.overview %> </p>' +
+    '<ul><li> Genres:<ul><li>' + 
+    '<% _.each(singleMovie.genres, function(el, i){ %>' +
+        '<a> <%= singleMovie.genres[i].name %> </a> ' +
+        '<% }) %> </li></ul>' +
+    '<li> Company:' + 
+        '<% _.each(singleMovie.companies, function(el, i){ %>' + 
+        '<%= singleMovie.companies[i] %>' +
+        '<% }) %> </li>' +
+    '<li>Budget:$ <%= singleMovie.budget %> </li>' +
+    '<li>Revenue:$ <%= singleMovie.revenue %> </li>' +
+    '<li> Actors: <ul>' + 
+        '<% _.each(singleMovie.actors, function(el, i){ %>' +
+        '<li> <img src="<%= smallImageUrl %>' + 
+        '<%= singleMovie.actors[i].profile_path %>">' +
+        '<span><%= singleMovie.actors[i].name %> as' +
+        '<%= singleMovie.actors[i].character %> </span>' +
+        '</li> <% }) %> </ul>'
+
+
+
+    ;
+    function renderSingleMoviePage(data) {
+        singleMovie = {
+            title : data.title,
+            poster: data.poster_path,
+            tagline : data.tagline,
+            rating : data.vote_average,
+            overview : data.overview,
+            year : parseInt(data.release_date),
+            budget : data.budget,
+            revenue : data.revenue,
+            companies : [],
+            countries : [],
+            similar: [],
+            runtime: data.runtime,
+            genres: data.genres,
+            images: [],
+            trailers: data.trailers.youtube,
+            actors: data.credits.cast
+        }
+        for (var country in data.production_countries) {
+            singleMovie.countries.push(data.production_countries[country].name)
+        }
+        for (var company in data.production_companies) {
+            singleMovie.companies.push(data.production_companies[company].name)
+        }
+        for (var movie in data.similar.results) {
+            singleMovie.similar.push({id:data.similar.results[movie].id, backdrop_path:data.similar.results[movie].backdrop_path, title:data.similar.results[movie].title})
+        }
+        for (var image in data.images.backdrops) {
+            singleMovie.images.push(data.images.backdrops[image].file_path)
+        }
+        return singleMovie
+    };
     $.ajax({
         type: "GET",
         //url: baseUrl + "/movie/" + id + '?api_key=' + apiKey,
-        url:'http://api.themoviedb.org/3/movie/13-forrest-gump?api_key=7a135ff2c408f8e138e4645f83b30222',
+        url:'http://api.themoviedb.org/3/movie/13-forrest-gump?api_key=7a135ff2c408f8e138e4645f83b30222&append_to_response=similar,images,trailers,credits',
         dataType: "json",
         success: function(data) {
-          renderSingleMoviePage(data) 
+            if(data.similar.results.length>10) {data.similar.results.splice(10)};
+            renderSingleMoviePage(data);//object that has all nesessary fields
+
+            $('#mainContent').find(':first-child').remove();
+    
+            $('#mainContent').append(_.template(singleMovieTemplate,renderSingleMoviePage(data)));
         }
     })
 
-    function renderSingleMoviePage(data) {
 
-    }
-        
+
+
 }
-
-
-//h1- original title
-//q - tagline
-//img - poster *
-//li - rating - stars
-//span*3 year, runtime, Country
-//p - owerwiew 
-//company, budget, revenue, (a - genres), (a -actors) 
-//button -watch trailer*
-//a>img - screenshots, popup
-//a - similar movies
