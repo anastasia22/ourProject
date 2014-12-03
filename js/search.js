785fddcea487ad44b5492e4340a0a8d037edfa79
Andrii Pylat <pilatao91@gmail.com>
2014-12-03 11:54:34 +0200

    changed main styles and header html
    Ну шо скажете по дизайну? Я ше не категоризував css, тільки закінчив із
    дизайном. ввечері дороблю все.

--- js/search.js
+++ js/search.js
@@ -1,23 +1,27 @@
+//search visualisation
 $(function() { 
     var state = true, frm;
-    $('<div>').attr('id', 'searchWrapper').addClass('search-wrapper').insertAfter('#mainMenu');
+    $('<div>').attr('id', 'searchWrapper').addClass('search-wrapper').insertAfter('#header');
+    
+    frm = document.createElement('form');
     
     
+    $('#searchWrapper').append(frm);
     
-    
+    //Toggle visibility of search block
     $('#callSearch').click(function() {
     if (state) {
         $('#searchWrapper').animate({
             'height': '100px',
             'opacity': '1',
             'margin-bottom': '10px'
-        }, 500);
+        }, 300);
     } else {
          $('#searchWrapper').animate({
             'height': '0px',
             'opacity': '0',
              'margin-bottom': '0px'
-        }, 500);
+        }, 300);
     }
     state = !state;
     });
